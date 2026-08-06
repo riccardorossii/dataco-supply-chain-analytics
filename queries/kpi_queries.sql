a561689b93d44e466e569d4a9cb766578fdc02db
riccardorossii <ricc.rossi2001@gmail.com>
2026-08-06 09:46:19 +0200

    Rimossi file pesanti (dataset, db) e aggiunto gitignore

--- queries/kpi_queries.sql
+++ queries/kpi_queries.sql
@@ -1,10 +1,5 @@
-SELECT DATE_FORMAT(STR_TO_DATE(order_date_dateorders, '%c/%e/%Y %H:%i'), '%Y-%m') AS mese,
-       ROUND(SUM(sales), 2) AS fatturato_totale
-FROM orders
-GROUP BY DATE_FORMAT(STR_TO_DATE(order_date_dateorders, '%c/%e/%Y %H:%i'), '%Y-%m')
-ORDER BY mese;
-
-SELECT customer_id, COUNT(*) as numero_ordini
-FROM orders
-GROUP BY customer_id
-ORDER BY numero_ordini desc LIMIT 10
+# Dati grezzi e database - non vanno su GitHub
+*.csv
+*.db
+__pycache__/
+*.pyc
