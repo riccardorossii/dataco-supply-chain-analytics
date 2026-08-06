f2d04390290f5620d3a19bb64903109bd96b9354
riccardorossii <ricc.rossi2001@gmail.com>
2026-08-06 08:52:54 +0200

    Aggiunta query top 10 clienti per numero ordini

--- queries/kpi_queries.sql
+++ queries/kpi_queries.sql
@@ -3,3 +3,8 @@ SELECT DATE_FORMAT(STR_TO_DATE(order_date_dateorders, '%c/%e/%Y %H:%i'), '%Y-%m'
 FROM orders
 GROUP BY DATE_FORMAT(STR_TO_DATE(order_date_dateorders, '%c/%e/%Y %H:%i'), '%Y-%m')
 ORDER BY mese;
+
+SELECT customer_id, COUNT(*) as numero_ordini
+FROM orders
+GROUP BY customer_id
+ORDER BY numero_ordini desc LIMIT 10
